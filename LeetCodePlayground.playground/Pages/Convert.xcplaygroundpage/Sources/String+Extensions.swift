2dae3d726c3c3d8fa7f82502ae22cc730ba33fc9
tiagoo <tiagoo@ciandt.com>
2023-01-12 13:27:37 -0300

    Inclusão de testes

---
+++ LeetCodePlayground.playground/Pages/Convert.xcplaygroundpage/Sources/String+Extensions.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+public extension String {
+
+    func toRawNumber() -> NSNumber? {
+        do {
+            var amountWithPrefix = self
+            let regex = try NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive)
+            amountWithPrefix = regex.stringByReplacingMatches(in: amountWithPrefix,
+                                                              options: NSRegularExpression.MatchingOptions(rawValue: 0),
+                                                              range: NSRange(location: 0, length: count),
+                                                              withTemplate: "")
+            if let double = Double(amountWithPrefix) {
+                let number = NSNumber(value: double / 100)
+                
+                guard number != 0 as NSNumber else {
+                    return nil
+                }
+                return number
+            }
+            return nil
+        } catch {
+            return nil
+        }
+    }
+
+}
