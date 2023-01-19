8a86ae07f367dbe51c9d16cf78ff26a2c9a5ea7a
tiagoo <tsabian@hotmail.com>
2023-01-19 12:53:22 -0300

    Update Test1.swift

--- LeetCodePlayground.playground/Pages/CoordinatorGenerics.xcplaygroundpage/Sources/Test1.swift
+++ LeetCodePlayground.playground/Pages/CoordinatorGenerics.xcplaygroundpage/Sources/Test1.swift
@@ -10,45 +10,45 @@ protocol QueueProtocol {
 }
 
 class IntQueue: QueueProtocol {
-    
     var items = [Int]()
-    
+
     func add(item: Int) {
         items.append(item)
     }
-    
+
     func getItem() -> Int? {
-        return items.count > 0 ? items.remove(at: 0) : nil
+        return !items.isEmpty ? items.remove(at: 0) : nil
     }
-    
+
     func count() -> Int {
         return items.count
     }
 }
 
 class GenericQueue<T>: QueueProtocol {
-
     var items = [T]()
 
     func add(item: T) {
         items.append(item)
     }
+
     func getItem() -> T? {
-        return items.count > 0 ? items.remove(at:0) : nil
+        return !items.isEmpty ? items.remove(at: 0) : nil
     }
+
     func count() -> Int {
         return items.count
     }
 }
 
-//var intQ = IntQueue()
-//intQ.add(item: 2)
-//intQ.add(item: 4)
-//print(intQ.getItem()!)
-//intQ.add(item: 6)
+// var intQ = IntQueue()
+// intQ.add(item: 2)
+// intQ.add(item: 4)
+// print(intQ.getItem()!)
+// intQ.add(item: 6)
 
-//var intQ2 = GenericQueue<Int>()
-//intQ2.add(item: 2)
-//intQ2.add(item: 4)
-//print(intQ2.getItem()!)
-//intQ2.add(item: 6)
+// var intQ2 = GenericQueue<Int>()
+// intQ2.add(item: 2)
+// intQ2.add(item: 4)
+// print(intQ2.getItem()!)
+// intQ2.add(item: 6)
