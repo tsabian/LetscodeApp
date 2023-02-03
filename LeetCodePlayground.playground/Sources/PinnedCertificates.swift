75ec2f29a00edd69c96e46d91e4959783b409895
Tiago Oliveira <tsabian@hotmail.com>
2023-02-03 09:06:00 -0300

    Update PinnedCertificates.swift

---
+++ LeetCodePlayground.playground/Sources/PinnedCertificates.swift
@@ -0,0 +1,31 @@
+import Alamofire
+import Foundation
+
+public struct ServerTrustPolicy: ServerTrustEvaluating {
+    let certificates: [SecCertificate]
+
+    init(certificates: [SecCertificate]) {
+        self.certificates = certificates
+    }
+
+    public func evaluate(_ trust: SecTrust, forHost host: String) throws {
+        let policy = SecPolicyCreateSSL(true, host as CFString)
+        SecTrustSetPolicies(trust, policy)
+
+        guard let serverCertificates = SecTrustCopyCertificateChain(trust) as? [SecCertificate], !serverCertificates.isEmpty else {
+            throw NSError()
+        }
+
+        let pinnedCertificatesData = certificates.compactMap { $0.data }
+
+        var trustIsValid = false
+        for serverCertificate in serverCertificates {
+            <#body#>
+        }
+
+        var policyError: CFError?
+        if !SecTrustEvaluateWithError(trust, &policyError) {
+            throw policyError!
+        }
+    }
+}
