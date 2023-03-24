c9f710b2be03395fedd1eca564003b0cda64e0e8
tiagoo <tiagoo@ciandt.com>
2023-03-24 17:21:47 -0300

    Update WeatherModel.swift

--- WeatherShared/Models/WeatherModel.swift
+++ WeatherShared/Models/WeatherModel.swift
@@ -12,6 +12,7 @@ import Foundation
 struct WeatherModel: Codable {
     let location: Location
     let current: Current
+    let forecast: Forecast?
 }
 
 // MARK: - Current
@@ -25,12 +26,12 @@ struct Current: Codable {
     let windMph, windKph: Double
     let windDegree: Int
     let windDir: String
-    let pressureMB, pressureIn, precipMm, precipIn: Int
-    let humidity, cloud, feelslikeC: Int
-    let feelslikeF: Double
+    let pressureMB: Int
+    let pressureIn, precipMm, precipIn: Double
+    let humidity, cloud: Int
+    let feelslikeC, feelslikeF: Double
     let visKM, visMiles, uv: Int
-    let gustMph: Double
-    let gustKph: Int
+    let gustMph, gustKph: Double
 
     enum CodingKeys: String, CodingKey {
         case lastUpdatedEpoch = "last_updated_epoch"
@@ -63,6 +64,135 @@ struct Current: Codable {
 struct Condition: Codable {
     let text, icon: String
     let code: Int
+}
+
+// MARK: - Forecast
+
+struct Forecast: Codable {
+    let forecastday: [Forecastday]
+}
+
+// MARK: - Forecastday
+
+struct Forecastday: Codable {
+    let date: String
+    let dateEpoch: Int
+    let day: Day
+    let astro: Astro
+    let hour: [Hour]
+
+    enum CodingKeys: String, CodingKey {
+        case date
+        case dateEpoch = "date_epoch"
+        case day, astro, hour
+    }
+}
+
+// MARK: - Astro
+
+struct Astro: Codable {
+    let sunrise, sunset, moonrise, moonset: String
+    let moonPhase, moonIllumination: String
+    let isMoonUp, isSunUp: Int
+
+    enum CodingKeys: String, CodingKey {
+        case sunrise, sunset, moonrise, moonset
+        case moonPhase = "moon_phase"
+        case moonIllumination = "moon_illumination"
+        case isMoonUp = "is_moon_up"
+        case isSunUp = "is_sun_up"
+    }
+}
+
+// MARK: - Day
+
+struct Day: Codable {
+    let maxtempC, maxtempF, mintempC, mintempF: Double
+    let avgtempC, avgtempF, maxwindMph, maxwindKph: Double
+    let totalprecipMm, totalprecipIn: Double
+    let totalsnowCM, avgvisKM, avgvisMiles, avghumidity: Int
+    let dailyWillItRain, dailyChanceOfRain, dailyWillItSnow, dailyChanceOfSnow: Int
+    let condition: Condition
+    let uv: Int
+
+    enum CodingKeys: String, CodingKey {
+        case maxtempC = "maxtemp_c"
+        case maxtempF = "maxtemp_f"
+        case mintempC = "mintemp_c"
+        case mintempF = "mintemp_f"
+        case avgtempC = "avgtemp_c"
+        case avgtempF = "avgtemp_f"
+        case maxwindMph = "maxwind_mph"
+        case maxwindKph = "maxwind_kph"
+        case totalprecipMm = "totalprecip_mm"
+        case totalprecipIn = "totalprecip_in"
+        case totalsnowCM = "totalsnow_cm"
+        case avgvisKM = "avgvis_km"
+        case avgvisMiles = "avgvis_miles"
+        case avghumidity
+        case dailyWillItRain = "daily_will_it_rain"
+        case dailyChanceOfRain = "daily_chance_of_rain"
+        case dailyWillItSnow = "daily_will_it_snow"
+        case dailyChanceOfSnow = "daily_chance_of_snow"
+        case condition, uv
+    }
+}
+
+// MARK: - Hour
+
+struct Hour: Codable {
+    let timeEpoch: Int
+    let time: String
+    let tempC, tempF: Double
+    let isDay: Int
+    let condition: Condition
+    let windMph, windKph: Double
+    let windDegree: Int
+    let windDir: String
+    let pressureMB: Int
+    let pressureIn, precipMm, precipIn: Double
+    let humidity, cloud: Int
+    let feelslikeC, feelslikeF, windchillC, windchillF: Double
+    let heatindexC, heatindexF, dewpointC, dewpointF: Double
+    let willItRain, chanceOfRain, willItSnow, chanceOfSnow: Int
+    let visKM, visMiles: Int
+    let gustMph, gustKph: Double
+    let uv: Int
+
+    enum CodingKeys: String, CodingKey {
+        case timeEpoch = "time_epoch"
+        case time
+        case tempC = "temp_c"
+        case tempF = "temp_f"
+        case isDay = "is_day"
+        case condition
+        case windMph = "wind_mph"
+        case windKph = "wind_kph"
+        case windDegree = "wind_degree"
+        case windDir = "wind_dir"
+        case pressureMB = "pressure_mb"
+        case pressureIn = "pressure_in"
+        case precipMm = "precip_mm"
+        case precipIn = "precip_in"
+        case humidity, cloud
+        case feelslikeC = "feelslike_c"
+        case feelslikeF = "feelslike_f"
+        case windchillC = "windchill_c"
+        case windchillF = "windchill_f"
+        case heatindexC = "heatindex_c"
+        case heatindexF = "heatindex_f"
+        case dewpointC = "dewpoint_c"
+        case dewpointF = "dewpoint_f"
+        case willItRain = "will_it_rain"
+        case chanceOfRain = "chance_of_rain"
+        case willItSnow = "will_it_snow"
+        case chanceOfSnow = "chance_of_snow"
+        case visKM = "vis_km"
+        case visMiles = "vis_miles"
+        case gustMph = "gust_mph"
+        case gustKph = "gust_kph"
+        case uv
+    }
 }
 
 // MARK: - Location
