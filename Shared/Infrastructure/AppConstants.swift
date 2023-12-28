0c221604caa649c62aadfe3c4d9f319f1a0fe084
Tiago A. de Oliveira <tiagoo@ciandt.com>
2023-12-28 13:27:22 -0300

    Inclui o teste de uma UITableView e um teste de GCD

--- Shared/Infrastructure/AppConstants.swift
+++ Shared/Infrastructure/AppConstants.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct AppConstants {
+enum AppConstants {
     static let salt = "22490F96"
     static let key = "89FDF1D18C5FFAEA89F141E77B22F1AF"
     static let iv = "D326DCE9D40D965B"
